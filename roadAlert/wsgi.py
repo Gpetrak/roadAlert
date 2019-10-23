e8a4382e747d4fd893152a66945ecfb9a3c19a61
gpetrak <gkpetrak@gmail.com>
2019-10-23 14:38:12 +0300

    fixing bugs

--- roadAlert/wsgi.py
+++ roadAlert/wsgi.py
@@ -17,7 +17,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # adjust the Python version in the line below as needed
-sys.path.append('~/envBot/lib/python3.5/site-packages')
+sys.path.append('~/envBot/lib/python2.7/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roadAlert.settings")
 
